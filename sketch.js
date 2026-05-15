311ad3bb4f3148b92d8786274ac74f33e2cce163
rowndill <rowndill@snu.ac.kr>
2026-05-15 15:37:20 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,9 +5,37 @@ function setup() {
 function draw() {
   background(220);
 
-  drawBackground1();
-  drawBackground2();
-  drawBackground3();
-  drawBackground4();
-  drawBackground5();
+  if (backgroundNum == 1){
+    drawBackground1();
+  }
+  if (backgroundNum == 2){
+    drawBackground2();
+  }
+  if (backgroundNum == 3){
+    drawBackground3();
+  }
+  if (backgroundNum == 4){
+    drawBackground4();
+  }
+  if (backgroundNum == 5){
+    drawBackground5();
+  }
+}
+
+function keyPressed(){
+  if (key=='1'){
+    backgroundNum = '1'
+  }
+  if (key=='2'){
+    backgroundNum = '2'
+  }
+  if (key=='3'){
+    backgroundNum = '3'
+  }
+  if (key=='4'){
+    backgroundNum = '4'
+  }
+  if (key=='5'){
+    backgroundNum = '5'
+  }
 }
