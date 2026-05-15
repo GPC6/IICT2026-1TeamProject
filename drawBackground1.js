594a0a9f6b51d9982bb11989085936570fe4d821
rowndill <rowndill@snu.ac.kr>
2026-05-15 15:25:25 +0900

    Create drawBackground1.js

---
+++ drawBackground1.js
@@ -0,0 +1,13 @@
+function setup(){
+    createCanvas(400, 400);
+}
+
+function draw(){
+    background(220);
+    fill(220, 50, 30);
+    ellipse(200, 200, 50, 50);
+    fill(30, 220, 50);
+    ellipse(100, 200, 50, 50);
+    fill(50, 30, 220);
+    ellipse(300, 200, 50, 50);
+}
